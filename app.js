cd34d92e207c99c3250a7da8a4dac886f745cb00
Anusha SP <anushaspga@gmail.com>
2021-10-09 23:43:32 +0530

    added changes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var outputdiv = document.querySelector("#output");
 //var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationUrl(){
-    return serverUrl + "?" + "text" + txtInput
+function getTranslationUrl(input){
+    return serverUrl+ "?" + "text=" + input
 }
 
 function errorHandler(error){
@@ -15,14 +15,14 @@ function errorHandler(error){
 }
 function clickHandler(){
   //  outputdiv.innerText = "ajsjsjsjsjsjsj "+txtInput.value;
-  var outputTxt = txtInput.value;
-  fetch(getTranslationUrl(outputTxt))
-  .then(respone => respone.json)
+  var inputTxt = txtInput.value;
+  fetch(getTranslationUrl(inputTxt))
+  .then(respone => respone.json())
   .then(json => {
-      var translatedText = json.contents.traslated;
+      var translatedText = json.contents.translated;
       outputdiv.innerText = translatedText;
   })
-  .catch(errorHandler);
+  .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler);
